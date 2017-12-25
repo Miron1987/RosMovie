82b18e984050d07e6872908e01bfcb30643a3a35
Aleksandr Kougia <zero0072002@mail.ru>
2017-12-25 18:53:09 +0300

    first commit

---
+++ RosMovies/RosMovies/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RosMovies
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
