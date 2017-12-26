7d23fdbf1d9adb7580140053a20184e8c2463fd9
Aleksandr Kougia <zero0072002@mail.ru>
2017-12-26 20:29:20 +0300

    fix

--- RosMovies/RosMovies/Controllers/HomeController.cs
+++ RosMovies/RosMovies/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RosMovies.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace RosMovies.Controllers
 {
@@ -17,18 +19,58 @@ namespace RosMovies.Controllers
             return View(db.Movies);
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Registration()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(User user)
         {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                var existingUser = db.Users.FirstOrDefault(u => u.Mail == user.Mail);
 
-            return View();
+                if (existingUser == null)
+                {
+                    db.Users.Add(new User
+                    {
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        Mail = user.Mail,
+                        Password = user.Password,
+                        Moderator = false
+                    });
+                    db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(user.Mail, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким e-mail уже существует");
+                    return View(user);
+                }
+            }
+            else
+            {
+                return View(user);
+            }
         }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
--- RosMovies/RosMovies/Models/Movie.cs
+++ RosMovies/RosMovies/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,14 +10,22 @@ namespace RosMovies.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Введите Название")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Введите Режиссера")]
         public string Director { get; set; }
 
+        [Required(ErrorMessage = "Введите Актеров")]
         public string Actors { get; set; }
 
+        [Required(ErrorMessage = "Введите описание")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Введите жанр")]
+        public string Genre { get; set; }
+
         public List<Review> Reviews { get; set; }
+
     }
 }
